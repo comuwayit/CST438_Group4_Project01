a5df5da9ca8a6ad9fd61dbe2110ee79a38958f0f
Osman <79539075+OsmanSafi@users.noreply.github.com>
2022-02-08 15:48:08 -0800

    Update UserDataBase.java

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserDataBase.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserDataBase.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 
 import com.daclink.drew.sp22.cst438_project01_starter.User;
 
-@Database(entities = {User.class}, version = 1); // idk what this error means :(
+@Database(entities = {User.class}, version = 1) // idk what this error means :(
 public abstract class UserDataBase extends RoomDatabase {
     public static final String DB_NAME = "USER_DATABASE";
     public static final String USER_TABLE = "USER_TABLE";
